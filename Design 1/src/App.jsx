2f6a3fb389041c533b237be74d735492b1dea432
Jay Solanki <jaysol8900@gmail.com>
2025-07-02 12:17:16 +0530

    section-3 added

--- Design 1/src/App.jsx
+++ Design 1/src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Section1 from './components/Section1'
 import Section2 from './components/Section2'
-
+import Section3 from './components/Section3'
 const App = () => {
   return (
     <div>
       <Navbar />
       <Section1 />
       <Section2 />
+      <Section3 />
     </div>
   )
 }
---
+++ Design 1/src/components/Section3.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import style from "./section3.module.css";
+const Section3 = () => {
+  return (
+    <div className={style.section3}>
+      <h1 className={style.h1}>Popular This Week</h1>
+      <div className={style.wrapper}>
+        <div className={style.image1}>
+          <div className={style.bottomdetail}>
+            <div className={style.left}>
+              <h3 className={style.lefth3}>C Costa</h3>
+              <p className={style.leftp}>Quries Yang</p>
+            </div>
+            <div className={style.right}>
+              <button className={style.rightbutton}>2.5 ETH</button>
+              <h3 className={style.righth3}>💖 45k</h3>
+            </div>
+          </div>
+        </div>
+        <div className={style.image2}>
+          <div className={style.bottomdetail}>
+            <div className={style.left}>
+              <h3 className={style.lefth3}>C Costa</h3>
+              <p className={style.leftp}>Quries Yang</p>
+            </div>
+            <div className={style.right}>
+              <button className={style.rightbutton}>2.5 ETH</button>
+              <h3 className={style.righth3}>💖 45k</h3>
+            </div>
+          </div>
+        </div>
+        <div className={style.image3}>
+          <div className={style.bottomdetail}>
+            <div className={style.left}>
+              <h3 className={style.lefth3}>C Costa</h3>
+              <p className={style.leftp}>Quries Yang</p>
+            </div>
+            <div className={style.right}>
+              <button className={style.rightbutton}>2.5 ETH</button>
+              <h3 className={style.righth3}>💖 45k</h3>
+            </div>
+          </div>
+        </div>
+        <div className={style.image4}>
+          <div className={style.bottomdetail}>
+            <div className={style.left}>
+              <h3 className={style.lefth3}>C Costa</h3>
+              <p className={style.leftp}>Quries Yang</p>
+            </div>
+            <div className={style.right}>
+              <button className={style.rightbutton}>2.5 ETH</button>
+              <h3 className={style.righth3}>💖 45k</h3>
+            </div>
+          </div>
+        </div>
+        <div className={style.image5}>
+          <div className={style.bottomdetail}>
+            <div className={style.left}>
+              <h3 className={style.lefth3}>C Costa</h3>
+              <p className={style.leftp}>Quries Yang</p>
+            </div>
+            <div className={style.right}>
+              <button className={style.rightbutton}>2.5 ETH</button>
+              <h3 className={style.righth3}>💖 45k</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Section3;
