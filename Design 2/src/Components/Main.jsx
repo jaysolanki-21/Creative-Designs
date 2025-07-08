ca4fdbcc84571f52f362da3c0962e4fb9be008b2
Jay Solanki <jaysol8900@gmail.com>
2025-07-08 16:20:38 +0530

    Section-1

---
+++ Design 2/src/Components/Main.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import style from "../Styles/main.module.css";
+const Main = () => {
+  return (
+    <div>
+      <div className={style.section1}>
+        <div className={style.img}>
+          <div className={style.nav}>
+            <div className={style.logo}>
+              {/* <img src="images/image (12).svg" alt="logo" /> */}
+              <span className={style.head}>luminious labs</span>
+            </div>
+            <div className={style.menu}>
+              <span>Technology</span>
+              <span>Company</span>
+              <span>Commercials</span>
+              <span>Blog</span>
+              <span>Pop-up Store</span>
+              <span>Shop</span>
+            </div>
+            <div className={style.cart}>
+              <span>Cart</span>
+              <span className={style.icon}>
+                <i class="ri-shopping-bag-line"></i>
+              </span>
+            </div>
+          </div>
+          <div className={style.hero}>
+            <h6>Red Light Therapy: proven,safe, and non-invasive</h6>
+            <h1>Your cells, supercharged</h1>
+          </div>
+        </div>
+      </div>
+     
+    </div>
+  );
+};
+
+export default Main;
